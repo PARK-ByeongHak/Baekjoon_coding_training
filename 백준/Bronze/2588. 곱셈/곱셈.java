12d03635bdb56551f8482da16999d0c80cfbc5dd
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-03-10 09:46:57 +0900

    [Bronze III] Title: 곱셈, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int A, B;	
+        
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        System.out.println(A*(B%10));
+        System.out.println(A*((B/10)%10));
+        System.out.println(A*(B/100));
+        System.out.println(A*B);
+
+	}
+
+}
