685b52e2be38e8084fb9e63b5ab9171d342c8397
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-03-11 16:00:29 +0900

    [Bronze V] Title: A+B - 8, Time: 276 ms, Memory: 18968 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int count, A, B;
+		
+		count = sc.nextInt();
+		for (int i = 1; i <= count; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println("Case #"+i+": "+A+" + "+B+" = "+(A+B));
+		}
+	}	
+}
+ 	
