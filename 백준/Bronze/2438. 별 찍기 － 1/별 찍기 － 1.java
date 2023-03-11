9cc06984dddce0cc7b32f5e3361f58263ce2381c
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-03-11 16:02:15 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 300 ms, Memory: 18112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int count;
+		
+		count = sc.nextInt();
+		for (int i = 1; i <= count; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}	
+}
+ 	
