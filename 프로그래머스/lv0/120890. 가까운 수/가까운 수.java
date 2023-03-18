e6ed176aee6fc3839bb6a4e1874066b339914450
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-03-18 14:58:43 +0900

    [level 0] Title: 가까운 수, Time: 0.33 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= n) {
+                int leftValue = n - array[(i - 1)];
+                int rightValue = array[i] - n;
+                return leftValue <= rightValue ? array[(i - 1)] : array[i];
+            }
+        }
+        return array[array.length - 1];
+    }
+}
