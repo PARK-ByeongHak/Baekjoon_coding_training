43211c6d589841bd6cb7a8afc14ff415e45ae61d
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-03-21 22:57:50 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[][] lines) {
+        
+        // 1. arr 배열 및 변수 초기화
+        int[] array = new int[200];
+        int answer = 0;
+        
+        // 2. lines 정보를 arr 배열에 적용
+        for(int i = 0; i < lines.length; i++){
+            for(int j = lines[i][0] + 100; j < lines[i][1] + 100; j++){
+                array[j]++;
+            }
+        }
+        // 3. arr 배열에서 겹친 부분 세기
+        for(int i = 0; i < 200; i++){
+            if(array[i] > 1){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
