1282381edc3b23fe731134a0c89dcd7678b2630b
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-04-07 17:30:35 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18236 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+    	
+    	String num = sc.next();
+    	System.out.println(":fan::fan::fan:");
+    	System.out.println(":fan::"+num+"::fan:");
+    	System.out.print(":fan::fan::fan:");
+    }
+}
