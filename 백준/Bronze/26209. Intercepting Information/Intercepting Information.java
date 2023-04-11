c87901dad8d8974338e4952d8807f66a1ee56e30
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-04-11 16:47:22 +0900

    [Bronze V] Title: Intercepting Information, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/26209. Intercepting Information/Intercepting Information.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        int g = sc.nextInt();
+        int h = sc.nextInt();
+        if(a==9||b==9||c==9||d==9||e==9||f==9||g==9||h==9) {
+        	System.out.println("F");
+        }else {
+        	System.out.println("S");
+        }
+        
+    }
+}
