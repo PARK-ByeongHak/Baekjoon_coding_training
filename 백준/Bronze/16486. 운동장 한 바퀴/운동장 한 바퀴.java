f8ccd0eacfc80a1676ce5b1bb33ecf2b47fb4409
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-04-12 18:18:27 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 208 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        double pi = 3.141592;
+        System.out.println(x*2 + 2*pi*y);
+
+    }
+}
