3ae8c5db5fe3ecc097ab5680fbd9045732a8a83b
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-04-20 21:54:52 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 284 ms, Memory: 19308 KB -BaekjoonHub

---
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    	int n = sc.nextInt();
+    	int m = sc.nextInt();
+
+    	int a = 100-n;
+    	int b = 100-m;
+    	int c = 100-(a+b);
+    	int d = a*b;
+    	int q = d/100;
+    	int r = d%100;
+    	
+    	System.out.println(a+" "+b+" "+c+" "+d+" "+q+" "+r);
+    	System.out.println((c+q)+" "+r);
+    }
+}
