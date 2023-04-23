b83989e175aeedda811ad77bb7ee7384e320e1e6
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-04-23 09:42:08 +0900

    [Bronze IV] Title: Oddities, Time: 232 ms, Memory: 18264 KB -BaekjoonHub

---
+++ 백준/Bronze/10480. Oddities/Oddities.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i=0; i<a; i++) {
+        	int num = sc.nextInt();
+        	if(Math.abs(num)%2==0) {
+        		System.out.println(num+" is even");
+        	}else {
+        		System.out.println(num+" is odd");
+        	}
+        }
+    }
+}
