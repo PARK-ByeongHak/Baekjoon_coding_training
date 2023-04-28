33846cb49f17a28db7416b0b19295737bbaf2ce0
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-04-28 20:22:09 +0900

    [Bronze IV] Title: Lots of Liquid, Time: 572 ms, Memory: 65964 KB -BaekjoonHub

---
+++ 백준/Bronze/25991. Lots of Liquid/Lots of Liquid.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        double sum = 0d;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (n-->0) {
+            double cur = Double.parseDouble(st.nextToken());
+            sum += Math.pow(cur, 3);
+        }
+        System.out.println(Math.cbrt(sum));
+    }
+}
