cd3d4d43559735a97f373ed3b973ee395481ace5
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-07 21:18:16 +0900

    [Bronze III] Title: 택시 기하학, Time: 216 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double n = sc.nextInt();
+		
+		System.out.printf("%.6f\n%.6f",n*n*Math.PI,n*n*2.000000);
+	}
+
+}
