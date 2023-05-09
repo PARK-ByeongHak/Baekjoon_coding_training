460d3eb804bdb93106413964d405f1712915554f
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-09 23:14:32 +0900

    [Bronze III] Title: TGN, Time: 228 ms, Memory: 17944 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if(a<b-c) {
+				System.out.println("advertise");
+			}else if(a==b-c) {
+				System.out.println("does not matter");
+			}else {
+				System.out.println("do not advertise");
+			}
+		}
+	}
+}
