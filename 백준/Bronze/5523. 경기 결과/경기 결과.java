ea4dd440fc59a09edbd8bcd88d3cf9c7448a4a71
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-16 21:41:44 +0900

    [Bronze III] Title: 경기 결과, Time: 1728 ms, Memory: 311428 KB -BaekjoonHub

---
+++ 백준/Bronze/5523. 경기 결과/경기 결과.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int aWin = 0;
+		int bWin = 0;
+
+		int caseCount = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < caseCount; i++) {
+			String str = sc.nextLine();
+			String[] temp = str.split(" ");
+
+			int aScore = Integer.parseInt(temp[0]);
+			int bScore = Integer.parseInt(temp[1]);
+
+			if (aScore > bScore)
+				aWin++;
+			else if (aScore < bScore)
+				bWin++;
+		}
+
+		System.out.println(aWin + " " + bWin);
+	}
+}
