cc6eaa521f3ad85de4c91ab8b70a3a74ba7e1ec8
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-18 18:50:49 +0900

    [Bronze III] Title: 계산기 프로그램, Time: 232 ms, Memory: 18152 KB -BaekjoonHub

---
+++ 백준/Bronze/5613. 계산기 프로그램/계산기 프로그램.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int sum = sc.nextInt();
+		while (true) {
+			String str = sc.nextLine();
+			if (str.equals("="))
+				break;
+			switch (str) {
+			case "+":
+				sum += sc.nextInt();
+				break;
+			case "-":
+				sum -= sc.nextInt();
+				break;
+			case "*":
+				sum *= sc.nextInt();
+				break;
+			case "/":
+				sum /= sc.nextInt();
+				break;
+			default:
+				break;
+			}
+		}
+		System.out.println(sum);
+	}
+}
