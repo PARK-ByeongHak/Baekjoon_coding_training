792aaa916c44529f05136b9e731d0a9961c6215e
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-19 20:34:07 +0900

    [Bronze III] Title: 신기한 수, Time: 396 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Bronze/17618. 신기한 수/신기한 수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+    	int a;
+    	int mok = 0, ext;
+    	int sum = 0;
+    	int count = 0;
+    	Scanner s = new Scanner(System.in);
+    	a = s.nextInt();
+    	
+    	for(int i=1; i<=a; i++) {
+    		mok = i;
+    		sum = 0;
+    		while(true) {
+    			
+    			if(mok == 0)
+    				break;
+    			ext = mok % 10;
+    			mok = mok / 10;
+    			
+    			sum = sum + ext;
+    		}
+    		
+    		if(i % sum == 0)
+    			count++;
+    	}
+    	System.out.println(count);
+    	s.close();
+    }
+}
