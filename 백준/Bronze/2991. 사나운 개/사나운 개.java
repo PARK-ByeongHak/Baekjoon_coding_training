47416316ba1685ce530d5eb0b9fa9563662a18d9
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-19 20:35:35 +0900

    [Bronze III] Title: 사나운 개, Time: 200 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2991. 사나운 개/사나운 개.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int[] Dog=new int[4];
+		int[] Man=new int[3];
+		int swp,x,jud;
+		for(int i=0;i<4;i++) {
+			Dog[i]=sc.nextInt();
+		}
+		for(int i=0;i<3;i++) {
+			Man[i]=sc.nextInt();
+		}
+		for(int i=0;i<3;i++) {
+			x=Man[i];
+			jud=0;
+			swp=0;
+			while(true) {
+				if(swp==0) {
+					x-=Dog[0];
+					if(x<=0) {
+						jud++;
+						break;
+					}
+					swp=1;
+				}
+				if(swp==1) {
+					x-=Dog[1];
+					if(x<=0) 
+						break;
+					swp=0;
+				}
+			}
+			x=Man[i];
+			swp=0;
+			while(true) {
+				if(swp==0) {
+					x-=Dog[2];
+					if(x<=0) {
+						jud++;
+						break;
+					}
+					swp=1;
+				}
+				if(swp==1) {
+					x-=Dog[3];
+					if(x<=0) 
+						break;
+					swp=0;
+				}
+			}
+			System.out.println(jud);
+		}
+	}
+}
