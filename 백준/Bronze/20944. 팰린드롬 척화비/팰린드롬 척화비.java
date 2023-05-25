fc867c282c1ccf4a088440ea2149e4e233770470
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-25 17:15:35 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 236 ms, Memory: 19744 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		char[] arr = new char[N];
+		Arrays.fill(arr, 'a');
+		System.out.println(arr);
+	}
+}
