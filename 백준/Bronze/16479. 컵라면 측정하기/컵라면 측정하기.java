54a0f68b5b9ae6e4a8781f069568d83600947593
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-05-29 11:38:41 +0900

    [Bronze III] Title: 컵라면 측정하기, Time: 216 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int K = sc.nextInt();
+        int D1 = sc.nextInt();
+        int D2 = sc.nextInt();
+
+        double h = Math.sqrt(Math.pow(K, 2) - Math.pow((D1 - D2) / 2.0, 2));
+        double result = Math.pow(h, 2);
+
+        System.out.println(result);
+    }
+}
