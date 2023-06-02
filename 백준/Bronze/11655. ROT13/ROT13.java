fedd010bc7d8597667b91fc4ed32879b864a0097
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-06-02 18:42:17 +0900

    [Bronze I] Title: ROT13, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c>='a' && c<='z'){
+                c += 13;
+                if (c>'z'){
+                    int tmp = c - 'z';
+                    c = 'a'-1;
+                    c += tmp;
+                }
+            } else if (c>='A' && c<='Z'){
+                c += 13;
+                if (c>'Z'){
+                    int tmp = c - 'Z';
+                    c = 'A'-1;
+                    c += tmp;
+                }
+            }
+            sb.append(c);
+        }
+        System.out.println(sb);
+    }
+}
