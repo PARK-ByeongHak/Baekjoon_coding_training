eb814c0a74049b976e4ca2c7dc24032c5dda1fcb
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-06-15 18:06:13 +0900

    [Bronze I] Title: 0의 개수, Time: 328 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11170. 0의 개수/0의 개수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+ 
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int cnt = 0;
+ 
+            for (int j = N; j <= M; j++) {
+                int t = j;
+                if (t == 0) {
+                    cnt++;
+                    continue;
+                }
+                while (t > 1) {
+                    if (t % 10 == 0) {
+                        cnt++;
+                    }
+                    t /= 10;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+}
