4e01d41e48a5aebfc371c904fe56c24010dcc082
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-06-15 18:13:19 +0900

    [Bronze V] Title: Арифметическая магия, Time: 116 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // 식은 x, y가 임의의 수일때, {(x+1)*(y+1)-xy-x-y}^n
+        // 정리해보면 x, y, n에 어떤 수가 와도 1^n, 즉 1이 됨
+        System.out.println("1");
+    }
+}
