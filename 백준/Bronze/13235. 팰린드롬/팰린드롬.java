e8f9a7b9c2c41f92748b79626026eb4546dda94b
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-06-29 17:23:27 +0900

    [Bronze II] Title: 팰린드롬, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String ... args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        boolean result = true;
+
+
+        //팰린드롬 검사
+        for(int idx= 0; idx<str.length()/2; idx++){
+            if(str.charAt(idx) == str.charAt(str.length()-idx-1)){
+                continue;
+            }else{
+                result = false;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
