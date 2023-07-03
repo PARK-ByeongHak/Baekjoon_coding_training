316c39864fe29b6d7f2e30f434e9e2dc49d083c2
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-07-03 17:48:06 +0900

    [Bronze I] Title: 희주의 수학시험, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/8974. 희주의 수학시험/희주의 수학시험.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int start = sc.nextInt();
+	int end = sc.nextInt();
+	int[] arr = new int[1001];
+	int sum = 0;
+	for(int i=0;i<arr.length;i++)
+	{
+		arr[i] = (((1)+(int)Math.sqrt(1+(8*(i-1))))/2);
+	}
+	for(int i=start;i<=end;i++)
+	{
+		sum += arr[i];
+	}
+	System.out.println(sum);
+}
+}
