890d255aa250b15d7a2c354f695c2dfb7d5a0dd1
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-08-03 18:42:14 +0900

    [Bronze III] Title: Ahmed Aly, Time: 260 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Bronze/10698. Ahmed Aly/Ahmed Aly.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		int N = Integer.parseInt(sc.nextLine());
+
+		for (int i = 0; i < N; i++) {
+			String[] str = sc.nextLine().split(" ");
+
+			int sum = 0;
+
+			int a = Integer.parseInt(str[0]);
+			int b = Integer.parseInt(str[2]);
+			int c = Integer.parseInt(str[4]);
+
+			if (str[1].equals("+")) {
+				sum = a + b;
+			} else if (str[1].equals("-")) {
+				sum = a - b;
+			}
+
+			System.out.println("Case " + (i + 1) + ": " + (sum == c ? "YES" : "NO"));
+		}
+		sc.close();
+	}
+}
