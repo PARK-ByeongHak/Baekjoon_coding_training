b75488217fd77ec18a32cf76ba52505c9275f52c
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-10-19 23:45:05 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int B = scanner.nextInt();
+
+        int A = B * 10 / 11;
+
+        System.out.println(A);
+    }
+}
