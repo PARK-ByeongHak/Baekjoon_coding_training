becf85c77d3fe4269c9ad17cf7afe74456b565d0
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-10-19 23:44:05 +0900

    [Bronze IV] Title: Kuber, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/20833. Kuber/Kuber.java
+++ 백준/Bronze/20833. Kuber/Kuber.java
@@ -1,14 +1,15 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int sum = 0;
-        for(int i=1; i<=a; i++) {
-        	sum+=(i*i*i);
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int totalCubes = 0;
+        for (int i = 1; i <= N; i++) {
+            totalCubes += i * i * i;
         }
-        System.out.println(sum);
+
+        System.out.println(totalCubes);
     }
 }
