00b8a58859637734411604148690fb6b98928a18
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-10-24 18:22:09 +0900

    [Bronze I] Title: 원의 분할, Time: 224 ms, Memory: 17832 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/16478. 원의 분할/원의 분할.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextLong();
+        long m = scanner.nextLong();
+        long k = scanner.nextLong();
+
+        double result = (n * k * 1.0) / m / 1.0;
+        System.out.printf("%.7f%n", result);
+    }
+}
