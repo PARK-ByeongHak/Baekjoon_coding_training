c5d1077fe54c88aa982f48ce8ee2aa98d4c31466
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-10-24 18:28:42 +0900

    [Bronze III] Title: Some Sum, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/21185. Some Sum/Some Sum.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        int givenN = Integer.parseInt(br.readLine());
+
+        String outputWord;
+        if (givenN % 4 == 0) {
+            outputWord = "Even";
+        } else if (givenN % 2 == 0) {
+            outputWord = "Odd";
+        } else {
+            outputWord = "Either";
+        }
+
+        System.out.print(outputWord);
+
+    }
+}
