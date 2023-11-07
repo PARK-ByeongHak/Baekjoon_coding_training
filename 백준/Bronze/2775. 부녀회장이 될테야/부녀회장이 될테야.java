4bd564bafc805d82c72f029e9366cb5118a462a5
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2023-11-07 18:57:44 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 232 ms, Memory: 18036 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -4,34 +4,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // 테스트 케이스 수 입력 받기
-        int testCase = sc.nextInt();
+        int T = sc.nextInt(); // 테스트 케이스 수
         
-        for (int i = 0; i < testCase; i++) {
-            // 층과 호수 입력 받기
-            int k = sc.nextInt(); // 층 수
-            int n = sc.nextInt(); // 호수 수
+        for (int t = 0; t < T; t++) {
+            int k = sc.nextInt(); // 층수
+            int n = sc.nextInt(); // 호수
             
-            // 거주민 수 계산 및 출력
-            int residents = calculateResidents(k, n);
-            System.out.println(residents);
-        }
-    }
-    
-    // 거주민 수를 계산하는 함수
-    public static int calculateResidents(int k, int n) {
-        // 0층의 경우 해당 호수의 값이 거주민 수
-        if (k == 0) {
-            return n;
-        }
-        
-        // k층의 n호 거주민 수 = (k-1)층의 1호부터 n호까지 거주민 수의 합
-        // 이를 재귀적으로 계산
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += calculateResidents(k - 1, i);
+            int[][] apt = new int[15][15]; // 아파트 정보 배열
+            
+            // 0층 초기화
+            for (int i = 1; i <= 14; i++) {
+                apt[0][i] = i;
+            }
+            
+            // 아파트 거주민 수 계산
+            for (int i = 1; i <= 14; i++) {
+                for (int j = 1; j <= 14; j++) {
+                    apt[i][j] = apt[i - 1][j] + apt[i][j - 1];
+                }
+            }
+            
+            System.out.println(apt[k][n]);
         }
         
-        return sum;
     }
 }
