855b2b6caad25b5ea59ff7a0ed51f53cfa6e455e
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-01-02 17:55:34 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException  {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        int endTime = 0;
+        int cowArr[][] = new int[N][2]; //[n][0] 도착시간, [n][1] 검문시간
+
+        for(int i=0;i<N;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            cowArr[i][0] = Integer.parseInt(st.nextToken());
+            cowArr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(cowArr, (o1,o2)-> o1[0]-o2[0]);
+        
+        for(int i=0;i<N;i++) {
+            if(endTime < cowArr[i][0]) {
+                endTime=cowArr[i][0]+cowArr[i][1];
+            }else {
+                endTime += cowArr[i][1];
+            }
+        }
+        bw.write(endTime+"");
+        bw.flush();
+        bw.close();
+    }
+}
