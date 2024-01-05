9e2f6508505c145703d7fc340eb011f6d06899c1
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-01-05 18:53:40 +0900

    [Silver IV] Title: 순서쌍의 곱의 합, Time: 292 ms, Memory: 23884 KB -BaekjoonHub

---
+++ 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    
+        int N = Integer.parseInt(in.readLine());
+        long[] num = new long[N];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+   
+        long sum = 0;
+        long n;
+        num[0] = Integer.parseInt(st.nextToken());
+        for(int i=1;i<N;i++){
+            n = Integer.parseInt(st.nextToken());
+            num[i] = n + num[i-1];
+            sum += num[i-1]*n;
+        }
+ 
+        System.out.println(sum);
+    }
+}
