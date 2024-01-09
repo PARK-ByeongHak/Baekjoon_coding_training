3dba082788a57375c0037719177a15bedc92a51d
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-01-09 18:51:25 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 440 ms, Memory: 35540 KB -BaekjoonHub

---
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        Map<String, Integer> map = new HashMap<>(n);
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        for (int i = 1; i < n; i++) {
+            String s = br.readLine();
+            if (map.get(s) == 1) {
+                map.remove(s);
+            } else {
+                map.put(s, map.get(s)-1);
+            }
+        }
+        for (String s : map.keySet()) {
+            System.out.print(s);
+        }
+    }
+}
