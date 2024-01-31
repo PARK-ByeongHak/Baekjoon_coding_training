923a7648ac040b37d08de3af717900b52f12f8c0
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-01-31 19:17:54 +0900

    [Silver IV] Title: 피사노 주기, Time: 148 ms, Memory: 15332 KB -BaekjoonHub

---
+++ 백준/Silver/9471. 피사노 주기/피사노 주기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        StringBuilder builder = new StringBuilder();
+        
+        for(int i = 0; i<n; i++) {
+            String[] tmp = reader.readLine().split(" ");
+            int m = Integer.parseInt(tmp[1]);
+            
+            int a = 1;
+            int b = 1;
+            int count = 0;
+            
+            while(true) {
+                int next = (a + b) % m;
+                a = b;
+                b = next;
+                count ++;
+                
+                if (a == 1 && b == 1) {
+                    break;
+                }
+            }
+            
+            builder.append(i + 1).append(" ").append(count).append("\n");
+        }
+    
+        System.out.println(builder);
+    }
+}
