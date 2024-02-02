f176025cd38ad14b2aa7cf7ab4e14e4dcd322733
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-02-02 19:04:55 +0900

    [Silver III] Title: 죽음의 게임, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		for(int i=0;i<N;++i) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int cnt = 0;
+		int idx = 0;// 0번부터시작
+		while(true) {
+			++cnt;
+			int val = arr[idx];
+			if(val == K) break;
+			idx= val;
+			if(cnt > N) break;
+		}
+		System.out.println(cnt > N ? -1 : cnt);
+	}
+
+}
