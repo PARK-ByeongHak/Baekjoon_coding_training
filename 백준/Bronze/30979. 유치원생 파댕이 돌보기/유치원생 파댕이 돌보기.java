8ce8840421a8f93275fab79c799cac77d3d2894d
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-02-27 19:15:54 +0900

    [Bronze III] Title: 유치원생 파댕이 돌보기, Time: 212 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/30979. 유치원생 파댕이 돌보기/유치원생 파댕이 돌보기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int N = sc.nextInt();
+        int[] candy = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            candy[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        for (int i : candy) {
+            sum += i;
+        }
+
+        if (T <= sum) {
+            System.out.println("Padaeng_i Happy");
+        } else {
+            System.out.println("Padaeng_i Cry");
+        }
+    }
+}
