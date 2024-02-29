85e8359fb1eddf3c062220ef21f86188dd25a2c9
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-02-29 16:09:49 +0900

    [Silver II] Title: 예쁜 케이크, Time: 344 ms, Memory: 20392 KB -BaekjoonHub

---
+++ 백준/Silver/24040. 예쁜 케이크/예쁜 케이크.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class Main{
+    public static void main(String[] args) {               
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0){
+            long N = sc.nextLong();
+            if(N%3==2 || N%9==0){
+                System.out.println("TAK");
+            }else{
+                System.out.println("NIE");
+            }
+        }
+        sc.close();
+    }
+}
