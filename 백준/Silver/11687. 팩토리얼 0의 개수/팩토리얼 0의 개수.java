86250e3acc6e828b8a0579cee6319d00f5973213
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-02-29 16:00:46 +0900

    [Silver I] Title: 팩토리얼 0의 개수, Time: 128 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Silver/11687. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.*;
+import java.util.Arrays;
+ 
+ 
+public class Main {
+ 
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        int m = Integer.parseInt(br.readLine());
+ 
+        int left=1;
+        int right=m*5;
+ 
+        boolean check=false;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(solve(mid)>m){
+                right=mid-1;
+            } else if(solve(mid)==m){
+                right=mid-1;
+                check=true;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+ 
+        if(check){
+            System.out.println(left);
+        }else{
+            System.out.println(-1);
+        }
+    }
+ 
+    private static int solve(int mid){
+        int count=0;
+ 
+        for(int i=5; i<=mid; i*=5){
+            count+=(mid/i);
+        }
+ 
+        return count;
+    }
+}
