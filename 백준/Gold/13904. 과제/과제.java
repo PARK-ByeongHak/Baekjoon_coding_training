1817d7336a4d246cccf9528be465d71313b14c5e
Park Byeong Hak <126544753+PARK-ByeongHak@users.noreply.github.com>
2024-04-26 19:09:23 +0900

    [Gold III] Title: 과제, Time: 144 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Gold/13904. 과제/과제.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		List<int[]> list = new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int d = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			list.add(new int[] {d,w});
+		}
+		
+		Collections.sort(list, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o2[1]-o1[1];
+			}
+		});
+		
+		int[] check = new int[1001];
+		for(int[] p : list) {
+			for(int i=p[0]; i>0; i--) {
+				if(check[i]==0) {
+					check[i] = p[1];
+					break;
+				}
+			}
+		}
+		
+		int total =0;
+		for(int num : check) {
+			total += num;
+		}
+		System.out.println(total);
+	}
+}
